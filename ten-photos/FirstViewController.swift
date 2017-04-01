a450c4ec5bdf875274e75caad37692af33156934
Mark Norgren <mark@marknorgren.com>
2017-03-31 22:47:13 -0500

    App takes 10 photos and saves to keychain

--- ten-photos/FirstViewController.swift
+++ ten-photos/FirstViewController.swift
@@ -12,9 +12,11 @@ class FirstViewController: UIViewController {
 
     var cameraController:CameraController!
 
+    let df = DateFormatter()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        df.dateFormat = "y.MM.dd'T'H.m.ss.SSS"
         cameraController = CameraController()
     }
 
@@ -22,14 +24,12 @@ class FirstViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         cameraController.startRunning()
-
     }
 
     fileprivate var fileWriteQueue:DispatchQueue = DispatchQueue(label: "com.markedsystems.file_write_queue",
                                                                attributes: [])
 
     @IBAction func handleShutterButton(_ sender: UIButton) {
-
         print("start")
         for i in 1...10 {
             print("taking \(i) photo")
@@ -45,35 +45,31 @@ class FirstViewController: UIViewController {
         print("done")
     }
 
-
     func saveImageToDisk(image: UIImage) {
-        // Create path.
         let date = Date()
-        let df = DateFormatter()
-        df.dateFormat = "y.MM.dd'T'H.m.ss.SSS"
+
         let dateString = df.string(from: date)
 
         // Save image.
         do {
-            let fileURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("\(dateString).jpg")
-            try UIImageJPEGRepresentation(image, 1)?.write(to: fileURL, options: .atomic)
+            let fileURL = try FileManager.default.url(for: .documentDirectory,
+                                                      in: .userDomainMask,
+                                                      appropriateFor: nil,
+                                                      create: false).appendingPathComponent("\(dateString).jpg")
+            guard let imageJPEG = UIImageJPEGRepresentation(image, 1) else {
+                fatalError("Could not create JPEG")
+            }
+            // Saves image data to documents folder, in-secure
+            // try imageJPEG.write(to: fileURL, options: .atomic)
+            DispatchQueue.main.async {
+                // Saves image data to keychain secure
+                let result = KeyChain.save(key: dateString, data: imageJPEG)
+                print("Saved: \(dateString) -> Keychain save result: \(result)")
+            }
         } catch {
             fatalError(error.localizedDescription)
         }
 
-    }
-
-    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafeRawPointer) {
-        guard error == nil else {
-            //Error saving image
-            return
-        }
-        //Image saved successfully
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
