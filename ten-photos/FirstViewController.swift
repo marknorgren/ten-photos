03f96fa8ee03e1bfcecb9242ad7e723c7a13bd5a
Mark Norgren <mark@marknorgren.com>
2017-03-31 21:53:50 -0500

    Writing ten photos to disk

--- ten-photos/FirstViewController.swift
+++ ten-photos/FirstViewController.swift
@@ -25,11 +25,50 @@ class FirstViewController: UIViewController {
 
     }
 
+    fileprivate var fileWriteQueue:DispatchQueue = DispatchQueue(label: "com.markedsystems.file_write_queue",
+                                                               attributes: [])
+
     @IBAction func handleShutterButton(_ sender: UIButton) {
-        cameraController.captureStillImage { (image, metadata) -> Void in
-            self.view.layer.contents = image
-            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+
+        print("start")
+        for i in 1...10 {
+            print("taking \(i) photo")
+            cameraController.captureStillImage { [weak self] (image, metadata) -> Void in
+                self?.view.layer.contents = image
+                self?.fileWriteQueue.async { [weak self] in
+                    self?.saveImageToDisk(image: image)
+                    print("wrote \(i) photo")
+                }
+            }
+            Thread.sleep(forTimeInterval: 0.5)
         }
+        print("done")
+    }
+
+
+    func saveImageToDisk(image: UIImage) {
+        // Create path.
+        let date = Date()
+        let df = DateFormatter()
+        df.dateFormat = "y.MM.dd'T'H.m.ss.SSS"
+        let dateString = df.string(from: date)
+
+        // Save image.
+        do {
+            let fileURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("\(dateString).jpg")
+            try UIImageJPEGRepresentation(image, 1)?.write(to: fileURL, options: .atomic)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+
+    }
+
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafeRawPointer) {
+        guard error == nil else {
+            //Error saving image
+            return
+        }
+        //Image saved successfully
     }
 
     override func didReceiveMemoryWarning() {
